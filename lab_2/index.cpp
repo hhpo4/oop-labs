96e8d362546cdedcaed8293ff14581d1428f68b6
Nikita Kolymazhnov <nikita.kolymazhnov@gmail.com>
2024-09-17 13:56:51 +0300

    ыва

--- lab_2/index.cpp
+++ lab_2/index.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 #define MAX_TECHNOLOGIES 50
+#define MAX_SITES 10
 
 struct Site {
     string name;
@@ -18,99 +19,17 @@ struct Site {
     string technologies[MAX_TECHNOLOGIES];
 };
 
-void readSitesFromFile(string filename, vector<Site>& sites) {
-    ifstream file(filename);
-
-    if (!file.is_open()) {
-        cerr << "Ошибка открытия файла: " << filename << endl;
-        return;
-    }
-
-    string line;
-    getline(file, line);
-
-    while (getline(file, line)) {
-        Site site;
-        
-        stringstream ss(line);
-        getline(ss, site.name, ',');
-        getline(ss, site.type, ',');
-        getline(ss, site.domain, ',');
-        getline(ss, line, ',');
-        site.users = stoi(line);
-        getline(ss, line, ',');
-        site.linked = stoi(line);
-        getline(ss, line, ',');
-        site.time = stod(line);
-
-        int techIndex = 0;
-        while (getline(ss, line, ',')) {
-            site.technologies[techIndex++] = line;
-        }
-
-        sites.push_back(site);
-    }
-
-    file.close();
-}
-
-void printSites(const vector<Site>& sites) {
-    for (const Site& site : sites) {
-        cout << "Название: " << site.name << endl;
-        cout << "Тип: " << site.type << endl;
-        cout << "Домен: " << site.domain << endl;
-        cout << "Пользователи: " << site.users << endl;
-        cout << "Ссылки: " << site.linked << endl;
-        cout << "Время загрузки: " << site.time << " сек." << endl;
-        cout << "Технологии: ";
-        for (int i = 0; i < MAX_TECHNOLOGIES; i++) {
-            if (site.technologies[i] != "") {
-                cout << site.technologies[i] << " ";
-            } else {
-                break;
-            }
-        }
-        cout << endl << endl;
-    }
-}
-
-Site findFastestSearch(const vector<Site>& sites) {
-    Site fastestSearch = sites[0]; 
-    for (const Site& site : sites) {
-        if (site.type == "Поисковый" && site.time < fastestSearch.time) {
-            fastestSearch = site;
-        }
-    }
-    return fastestSearch;
-}
-
-void printSitesWithMinUsers(const vector<Site>& sites, int minUsers) {
-    cout << "Информация о ресурсах с числом пользователей больше " << minUsers << ":\n";
-    for (const Site& site : sites) {
-        if (site.users > minUsers) {
-            cout << "Название: " << site.name << endl;
-            cout << "Тип: " << site.type << endl;
-            cout << "Домен: " << site.domain << endl;
-            cout << "Пользователи: " << site.users << endl;
-            cout << "Ссылки: " << site.linked << endl;
-            cout << "Время загрузки: " << site.time << " сек." << endl;
-            cout << "Технологии: ";
-            for (int i = 0; i < MAX_TECHNOLOGIES; i++) {
-                if (site.technologies[i] != "") {
-                    cout << site.technologies[i] << " ";
-                } else {
-                    break;
-                }
-            }
-            cout << endl << endl;
-        }
-    }
-}
+void readSitesFromFile(string filename, Site sites[]);
+void printSites(Site sites[]); 
+Site findFastestSearch(Site sites[]);
+void printSitesWithMinUsers(Site sites[], int minUsers);
 
 int main() {
-    vector<Site> sites;
+    Site* sites = new Site[MAX_SITES];
 
     readSitesFromFile("websites.txt", sites);
+    
+    printSites(sites);
 
     Site fastestSearch = findFastestSearch(sites);
     cout << "Доменное имя самого быстрого поискового сервиса: " << fastestSearch.domain << endl << endl;
@@ -121,7 +40,96 @@ int main() {
 
     printSitesWithMinUsers(sites, minUsers);
 
-    printSites(sites);
+    delete[] sites;
 
     return 0;
+}
+
+void readSitesFromFile(string filename, Site sites[]) {
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cerr << "Ошибка открытия файла: " << filename << endl;
+        return;
+    }
+
+    string line;
+    getline(file, line);
+
+    int index = 0;
+    while (index < MAX_SITES && getline(file, line)) {
+        stringstream ss(line);
+        getline(ss, sites[index].name, ',');
+        getline(ss, sites[index].type, ',');
+        getline(ss, sites[index].domain, ',');
+        getline(ss, line, ',');
+        sites[index].users = stoi(line);
+        getline(ss, line, ',');
+        sites[index].linked = stoi(line);
+        getline(ss, line, ',');
+        sites[index].time = stod(line);
+
+        int techIndex = 0;
+        while (techIndex < MAX_TECHNOLOGIES && getline(ss, line, ',')) {
+            sites[index].technologies[techIndex++] = line;
+        }
+
+        index++;
+    }
+
+    file.close();
+}
+
+void printSites(Site sites[]) {
+    for (int i = 0; i < MAX_SITES; i++) {
+        if (sites[i].name != "") {
+            cout << "Название: " << sites[i].name << endl;
+            cout << "Тип: " << sites[i].type << endl;
+            cout << "Домен: " << sites[i].domain << endl;
+            cout << "Пользователи: " << sites[i].users << endl;
+            cout << "Ссылки: " << sites[i].linked << endl;
+            cout << "Время загрузки: " << sites[i].time << " сек." << endl;
+            cout << "Технологии: ";
+            for (int j = 0; j < MAX_TECHNOLOGIES; j++) {
+                if (sites[i].technologies[j] != "") {
+                    cout << sites[i].technologies[j] << " ";
+                } else {
+                    break;
+                }
+            }
+            cout << endl << endl;
+        }
+    }
+}
+
+Site findFastestSearch(Site sites[]) {
+    Site fastestSearch = sites[0];
+    for (int i = 0; i < MAX_SITES; i++) {
+        if (sites[i].name != "" && sites[i].type == "Поисковый" && sites[i].time < fastestSearch.time) {
+            fastestSearch = sites[i];
+        }
+    }
+    return fastestSearch;
+}
+
+void printSitesWithMinUsers(Site sites[], int minUsers) {
+    cout << "Информация о ресурсах с числом пользователей больше " << minUsers << ":\n";
+    for (int i = 0; i < MAX_SITES; i++) {
+        if (sites[i].name != "" && sites[i].users > minUsers) {
+            cout << "Название: " << sites[i].name << endl;
+            cout << "Тип: " << sites[i].type << endl;
+            cout << "Домен: " << sites[i].domain << endl;
+            cout << "Пользователи: " << sites[i].users << endl;
+            cout << "Ссылки: " << sites[i].linked << endl;
+            cout << "Время загрузки: " << sites[i].time << " сек." << endl;
+            cout << "Технологии: ";
+            for (int j = 0; j < MAX_TECHNOLOGIES; j++) {
+                if (sites[i].technologies[j] != "") {
+                    cout << sites[i].technologies[j] << " ";
+                } else {
+                    break;
+                }
+            }
+            cout << endl;
+        }
+    }
 }
